435aeaf87f74eccc78ab1ef6d04a68282cc73195
aadil-nv <adilev2000@gmail.com>
2025-05-10 16:23:06 +0530

    3 heading created

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div>
         <h1 className='text-3xl text-red-700'>Home</h1>
+        <h1 className='text-3xl text-red-700'>Home</h1>
+        <h1 className='text-3xl text-red-700'>Home</h1>
     </div>
   )
 }
