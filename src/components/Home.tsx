4694c1d65e0678eef402075fab9c176f968fe676
aadil-nv <adilev2000@gmail.com>
2025-05-10 16:20:58 +0530

    Headin created

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-        <h1 className='text-3xl text-red-700'>Navbar</h1>
+        <h1 className='text-3xl text-red-700'>Home</h1>
     </div>
   )
 }
