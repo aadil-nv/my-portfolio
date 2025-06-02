3b0e4bce02faeda9f5aacf6e4e37862a6263b32e
aadil-nv <adilev2000@gmail.com>
2025-06-02 16:22:22 +0530

    dont make any changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,8 +2,8 @@
 export default function Home() {
   return (
     <div>
-        <h1 className='text-3xl text-red-700'>Home</h1>
-        <h1 className='text-3xl text-red-700'>Home</h1>
+        <h1 className='text-3xl text-red-700'>Muhammed Aadil Nv</h1>
+        <h1 className='text-3xl text-red-700'>Full Stack developer </h1>
         <h1 className='text-3xl text-red-700'>Home</h1>
     </div>
   )
